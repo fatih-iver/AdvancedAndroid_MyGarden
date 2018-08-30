7b8261032eda1bba26b35466a9d9d7782949ca39
fatih-iver-2106400264 <fatih.iver@boun.edu.tr>
2018-08-31 02:03:58 +0300

    Add Watering Service

---
+++ app/src/main/java/com/example/android/mygarden/PlantWateringService.java
@@ -0,0 +1,47 @@
+package com.example.android.mygarden;
+
+import android.app.IntentService;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
+import com.example.android.mygarden.provider.PlantContract;
+import com.example.android.mygarden.utils.PlantUtils;
+
+public class PlantWateringService extends IntentService {
+
+    public static final String ACTION_WATER_PLANTS = "com.example.android.mygarden.action.water_plants";
+
+    public PlantWateringService() {
+        super("PlantWateringService");
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        if(intent != null) {
+            final String action = intent.getAction();
+            if(action.equals(ACTION_WATER_PLANTS)){
+                handleActionWaterPlants();
+            }
+        }
+    }
+
+    private void handleActionWaterPlants() {
+        Uri contentUri = PlantContract.PlantEntry.CONTENT_URI;
+        ContentValues contentValues = new ContentValues();
+        long timeNow = System.currentTimeMillis();
+        contentValues.put(PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME, timeNow);
+        getContentResolver().update(contentUri,
+                contentValues,
+                PlantContract.PlantEntry.COLUMN_LAST_WATERED_TIME + ">?",
+                new String[]{String.valueOf(timeNow - PlantUtils.MAX_AGE_WITHOUT_WATER)});
+    }
+
+    public static void startActionWaterPlants(Context context) {
+        Intent intent = new Intent(context, PlantWateringService.class);
+        intent.setAction(ACTION_WATER_PLANTS);
+        context.startService(intent);
+    }
+}
